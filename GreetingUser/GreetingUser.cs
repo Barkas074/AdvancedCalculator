f7d61b8ba9ba649feda4de194535fe37fcb01ae9
Denis <Barkas074@yandex.ru>
2020-09-16 18:43:22 +0500

    Перенесена проверка вводимых данных в отдельный метод.

--- GreetingUser/GreetingUser.cs
+++ GreetingUser/GreetingUser.cs
@@ -6,58 +6,12 @@ namespace GreetingUser
 	{
 		static void Main(string[] args)
 		{
-			int birthDay;
-			int birthMonth;
-			int birthYear;
 			DateTime birthDate;
 			Console.WriteLine("Привет! Как тебя зовут?");
 			string nameUser = Console.ReadLine();
-			Console.WriteLine("Какого числа у тебя день рождения?");
-			//Проверяем вносимые данные
-			try
-			{
-				birthDay = int.Parse(Console.ReadLine());
-				while (birthDay < 1 || birthDay > 31)
-				{
-					Console.WriteLine("В месяце могут быть числа от 1 до 31!");
-					birthDay = int.Parse(Console.ReadLine());
-				}
-				Console.WriteLine("Месяц?");
-				birthMonth = Convert.ToInt32(Console.ReadLine());
-				while (birthMonth < 1 || birthMonth > 12)
-				{
-					Console.WriteLine("В году только 12 месяцев!");
-					birthMonth = Convert.ToInt32(Console.ReadLine());
-				}
-				Console.WriteLine("Год?");
-				birthYear = Convert.ToInt32(Console.ReadLine());
-				if (birthYear < (DateTime.Today.Year - 122))
-				{
-					Console.WriteLine("Официально самому старейшему человеку в мире было 122 года. И не говори, что ты Ли Цинъюнь. Укажи правильный год:");
-				}
-				if (birthYear >= DateTime.Today.Year)
-				{
-					Console.WriteLine("Так и поверил. Укажи правильный год:");
-				}
-				while (birthYear < (DateTime.Today.Year - 122) || birthYear >= DateTime.Today.Year)
-				{
-					birthYear = Convert.ToInt32(Console.ReadLine());
-				}
-				birthDay = ValidateDate(birthYear, birthMonth, birthDay);
-				birthDate = new DateTime(birthYear, birthMonth, birthDay);
-			}
-			catch (FormatException e)
-			{
-				Console.WriteLine("А вот так делать не стоило!");
-				return;
-			}
-			catch (ArgumentOutOfRangeException e)
-			{
-				Console.WriteLine("А вот так делать не стоило!");
-				return;
-			}
+			birthDate = InputDate();
 			//Определение возраста
-			int age = DateTime.Today.Year - birthYear;
+			int age = DateTime.Today.Year - birthDate.Year;
 			if (birthDate > DateTime.Today.AddYears(-age))
 			{
 				age--;
@@ -113,10 +67,58 @@ namespace GreetingUser
 			
 		}
 
-		//static int InputDate()
-		//{
-		//	return 0;
-		//}
+		static DateTime InputDate()
+		{
+			//Проверяем вносимые данные
+			int birthDay;
+			int birthMonth;
+			int birthYear;
+			DateTime birthDate = new DateTime();
+			Console.WriteLine("Какого числа у тебя день рождения?");
+			try
+			{
+				birthDay = int.Parse(Console.ReadLine());
+				while (birthDay < 1 || birthDay > 31)
+				{
+					Console.WriteLine("В месяце могут быть числа от 1 до 31!");
+					birthDay = int.Parse(Console.ReadLine());
+				}
+				Console.WriteLine("Месяц?");
+				birthMonth = Convert.ToInt32(Console.ReadLine());
+				while (birthMonth < 1 || birthMonth > 12)
+				{
+					Console.WriteLine("В году только 12 месяцев!");
+					birthMonth = Convert.ToInt32(Console.ReadLine());
+				}
+				Console.WriteLine("Год?");
+				birthYear = Convert.ToInt32(Console.ReadLine());
+				if (birthYear < (DateTime.Today.Year - 122))
+				{
+					Console.WriteLine("Официально самому старейшему человеку в мире было 122 года. И не говори, что ты Ли Цинъюнь. Укажи правильный год:");
+				}
+				if (birthYear >= DateTime.Today.Year)
+				{
+					Console.WriteLine("Так и поверил. Укажи правильный год:");
+				}
+				while (birthYear < (DateTime.Today.Year - 122) || birthYear >= DateTime.Today.Year)
+				{
+					birthYear = Convert.ToInt32(Console.ReadLine());
+				}
+				birthDay = ValidateDate(birthYear, birthMonth, birthDay);
+				birthDate = new DateTime(birthYear, birthMonth, birthDay);
+			}
+			catch (FormatException e)
+			{
+				Console.WriteLine("А вот так делать не стоило!");
+				Environment.Exit(0);
+			}
+			catch (ArgumentOutOfRangeException e)
+			{
+				Console.WriteLine("А вот так делать не стоило!");
+				Environment.Exit(0);
+			}
+			return birthDate;
+		}
 
 	}
 
