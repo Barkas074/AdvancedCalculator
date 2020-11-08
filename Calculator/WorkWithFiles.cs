b4f7d211f5d12040a2d9899821cc2ca8c0ac4531
Denis <Barkas074@yandex.ru>
2020-11-09 02:08:33 +0500

    Реализовал ОПЗ

--- Calculator/WorkWithFiles.cs
+++ Calculator/WorkWithFiles.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace Calculator
@@ -15,7 +16,7 @@ namespace Calculator
 			Console.WriteLine("Текст записан в файл.");
 		}
 
-		public string[] ReadTextFromFile(string path, string nameFile)
+		public List<string> ReadTextFromFile(string path, string nameFile)
 		{
 			using FileStream fStream = File.OpenRead(path + nameFile);
 			byte[] array = new byte[fStream.Length];
@@ -25,33 +26,35 @@ namespace Calculator
 			return ProcessingString(textFromFile);
 		}
 
-		string[] ProcessingString(string textFromFile)
+		List<string> ProcessingString(string textFromFile)
 		{
 			textFromFile = textFromFile.Replace("\t", string.Empty);
 			string[] text = textFromFile.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-			CheckingText(text);
-			return text;
-		}
-
-		void CheckingText(string[] text)
-		{
-			List<char> dictionarySymbols = new List<char>() { '*', '/', '+', '-' };
-			bool correctText = true;
+			List<string> dictionaryOperations = new List<string>() { "*", "/", "+", "-", "^", "(", ")", "x", "log", "sin", "cos", "arcsin", "arccos", "tg", "arctg" };
+			List<string> textExpression = new List<string>();
+			string temp = string.Empty;
 			for (int i = 0; i < text.Length; i++)
 			{
-				if (i < 2)
-					correctText = float.TryParse(text[i], out float x);
+				if (float.TryParse(text[i], out _) || dictionaryOperations.Contains(text[i]))
+					textExpression.Add(text[i]);
 				else
-					correctText = dictionarySymbols.Contains(char.Parse(text[i]));
-				if (!correctText)
-				{
-					Console.WriteLine("Выражение записано неверно.");
-					//if (ReadAnswer())
-						
-					//else
-						Environment.Exit(0);
-				}
+					for (int j = 0; j < text[i].Length; j++)
+					{
+						if (dictionaryOperations.Contains(text[i][j].ToString()) && temp != string.Empty)
+						{
+							Console.WriteLine("Выражение неверное!");
+							Environment.Exit(1);
+						}
+						temp += text[i][j];
+						if (float.TryParse(temp, out _) || dictionaryOperations.Contains(temp))
+						{
+							textExpression.Add(temp);
+							temp = string.Empty;
+						}
+					}
+					temp = string.Empty;
 			}
+			return textExpression;
 		}
 
 		static bool ReadAnswer()
